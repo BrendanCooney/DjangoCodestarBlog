bedde0938f4deca7e5bc35c875f3355f67c8111f
BrendanCooney <advicast@gmail.com>
2023-08-02 07:44:36 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-brendancoon-djangocodes-hppesxfzz5n.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['bc-codestar-1.herokuapp.com','localhost', '8000-brendancoon-djangocodes-hppesxfzz5n.ws-eu102.gitpod.io']
 
 
 # Application definition
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'blog',
 ]
 
