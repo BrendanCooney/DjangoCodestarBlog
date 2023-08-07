9b5e7487b2275ae6af288f191bb5ccf82f561d5c
BrendanCooney <advicast@gmail.com>
2023-08-07 22:08:23 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['bc-codestar-1.herokuapp.com','localhost', '8000-brendancoon-djangocodes-hppesxfzz5n.ws-eu102.gitpod.io','bc-codestar-1-3ce578f765f8.herokuapp.com', '8000-brendancoon-djangocodes-tlck2f1fqc5.ws-eu102.gitpod.io']
 
